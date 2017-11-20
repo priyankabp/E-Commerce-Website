9621d95f0d7c5d5b15706ce58e1e4e4b19ee26f5
Priyanka Phapale <pbphapale@gmail.com>
2017-11-19 19:38:28 -0500

    Displaying Thankyou page with order details and saving in transactions

--- cart.php
+++ cart.php
@@ -114,7 +114,7 @@
 				        <form action="thankyou.php" method="post" id="payment-form">
 				        	<span class="bg-warning" id="payment-errors"></span>
 				        	<input type="hidden" name="tax" value="<?=$tax;?>">
-				        	<input type="hidden" name="sub_total" value="<?=$sub_total;?>">
+				        	<input type="hidden" name="sub_total" value="<?=$subtotal;?>">
 				        	<input type="hidden" name="grand_total" value="<?=$grand_total;?>">
 				        	<input type="hidden" name="cart_id" value="<?=$cart_id;?>">
 				        	<input type="hidden" name="description" value="<?=$item_count.' item'.(($item_count>1)?'s':'').' from E-Farmarket !';?>">
--- thankYou.php
+++ thankYou.php
@@ -2,22 +2,30 @@
 	require_once 'core/init.php';
 
 	// get post data
-	$full_name = sanitize($_POST['full_name']);
-	$email = sanitize($_POST['email']);
-	$street = sanitize($_POST['street']);
-	$street2 = sanitize($_POST['street2']);
-	$city = sanitize($_POST['city']);
-	$state = sanitize($_POST['state']);
-	$zip_code = sanitize($_POST['zip_code']);
-	$country = sanitize($_POST['country']);
-	$tax = sanitize($_POST['tax']);
-	$sub_total = sanitize($_POST['sub_total']);
-	$grand_total = sanitize($_POST['grand_total']);
-	$cart_id = sanitize($_POST['cart_id']);
-	$description = sanitize($_POST['description']);
+	$full_name = $_POST['full_name'];
+	$email = $_POST['email'];
+	$street = $_POST['street'];
+	$street2 = $_POST['street2'];
+	$city = $_POST['city'];
+	$state = $_POST['state'];
+	$zip_code = $_POST['zip_code'];
+	$country = $_POST['country'];
+	$tax = $_POST['tax'];
+	$subtotal = $_POST['sub_total'];
+	$grand_total = $_POST['grand_total'];
+	$cart_id = $_POST['cart_id'];
+	$description = $_POST['description'];
 
 	$db->query("UPDATE cart SET paid = 1 WHERE id = '$cart_id'");
-	$db->query("INSERT INTO `e_commerce`.`transactions` (`cart_id`,`full_name`,`email`,`street`,`street2`,`city`,`state`,`zip_code`,`country`,`sub_total`,`tax`,`grand_total`,`description`) VALUES ('$cart_id','$full_name','$email','$street','$street2','$city','$state','$zip_code','$country','$sub_total','$tax','$grand_total','$description')");
+	$transaction_query = "INSERT INTO `transactions` (`cart_id`,`full_name`,`email`,`street`,`street2`,`city`,`state`,`zip_code`,`country`,`sub_total`,`tax`,`grand_total`,`description`) VALUES ('$cart_id','$full_name','$email','$street','$street2','$city','$state','$zip_code','$country','$subtotal','$tax','$grand_total','$description')";
+
+	if (mysqli_query($db,$transaction_query)) {
+		echo "Transaction complete";
+	}
+	else{
+		echo "Error: " . $transaction_query . "<br>" . $db->error;
+        echo "Transaction not complete";
+	}
 
 	$domain = ($_SERVER['HTTP_HOST'] != 'localhost')?'.'.$_SERVER['HTTP_HOST']:false;
 	setcookie(CART_COOKIE,'',1,'/',$domain,false);
@@ -34,7 +42,7 @@
 <address>
 	<?php echo $full_name; ?><br>
 	<?php echo $street; ?><br>
-	<?php echo (($street2 != '')?$street2.'<br>':''); ?><br>
+	<?php echo (($street2 != '')?$street2.'<br>':''); ?>
 	<?php echo $city.','.$state.'-'.$zip_code; ?><br>
 	<?php echo $country; ?>
 </address>
